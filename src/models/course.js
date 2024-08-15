b8e21a0cbcc82670970430591d20fe6c9585f1e5
oswaldo-mendez <ing.oswaldomendez@gmail.com>
2024-08-14 20:53:51 -0500

    crear models

---
+++ src/models/course.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Course = sequelize.define('Course', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+  },
+});
+
+module.exports = Course;
